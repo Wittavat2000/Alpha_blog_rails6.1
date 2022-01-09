c3e97123c8255c174a0ad06cec2b82a81df4fd7f
wittavat <Wittavat.s@ku.th>
2022-01-09 13:07:31 +0700

    Alter object state before_save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
     has_many :articles
     validates :username, presence: true, uniqueness: {case_sensitve: false},length: { minimum: 3 , maximum: 25 }
     VALIDATION_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
