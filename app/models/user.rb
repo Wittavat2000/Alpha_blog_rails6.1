83a505f97365a487d3925763e6a1339144cf7be6
wittavat <Wittavat.s@ku.th>
2022-01-09 13:43:44 +0700

    Add secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
     VALIDATION_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, uniqueness: {case_sensitve: false} , length: { maximum: 105 },
     format: { with: VALIDATION_EMAIL_REGEX }
-
+    has_secure_password
 end
