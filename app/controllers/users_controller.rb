c72f043d5585c07f202a5fd87054cc93812d56f9
wittavat <Wittavat.s@ku.th>
2022-01-11 20:18:59 +0700

    Add users index - Cleanup layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
       @articles = @user.articles
     end
 
+    def index
+      @users = User.all
+    end
+
     def new
       @user = User.new
     end
@@ -13,7 +17,7 @@ class UsersController < ApplicationController
       @user = User.new(user_params)
       if @user.save
         flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
-        redirect_to articles_path
+        redirect_to @user
       else
         render 'new'
       end
